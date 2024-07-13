cd45ae10e933d2b3d3c89beb02bf43771cdc0d5a
ADARSH-IIIT <adarshtech251@gmail.com>
2024-07-13 18:33:57 +0530

    first commit

---
+++ swiper_logic.js
@@ -0,0 +1,83 @@
+const animator = gsap 
+
+
+
+
+let box1 = animator.to('.swiper-box1'  ,  {
+
+    duration : 1 , 
+
+    transform : "translate( -100% , 0)" ,
+
+    // backgroundColor : 'blue'
+
+}).pause()
+
+
+
+
+
+let box3 = animator.to('.swiper-box3'  ,  {
+
+    duration : 1 , 
+
+    transform : "translate( 100% , 0)" ,
+
+    // backgroundColor : 'blue'
+
+}).pause()
+
+
+
+
+let box2 = animator.to('.swiper-box2>div'  ,  {
+
+    duration : .4 , 
+
+    flexGrow : 1 ,
+
+    fontSize : '6rem' ,
+
+    color : 'white'
+
+
+}).pause()
+
+
+
+
+
+function show_navbar(){
+    box1.play()
+    box2.play()
+    box3.play()
+}
+
+
+function hide_navbar(){
+    box1.reverse()
+    box2.reverse()
+    box3.reverse()
+}
+
+
+
+function toggle_navbar(event){
+
+      event.currentTarget.isopen = event.currentTarget.isopen ?? false
+
+      if(!event.currentTarget.isopen){
+          show_navbar()
+
+          event.currentTarget.isopen = !event.currentTarget.isopen
+      }
+
+      else{
+        hide_navbar()
+        event.currentTarget.isopen = !event.currentTarget.isopen
+      }
+}
+
+
+
+document.getElementById('logo').addEventListener('click' ,  toggle_navbar)
